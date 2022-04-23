eaf4659ec0f90a90a7fde1a714aaa4b6d4909e2f
Ganeshreddy chimmula <ganesh.rc@surya-soft.com>
2022-04-23 11:01:04 +0530

    Create dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { ReactElement, useState } from "react";
 import { Box } from "@mui/material";
+import { Gantt, Task, ViewMode } from "gantt-task-react";
 
 const App = (): React.ReactElement => {
+  const [view, setView] = useState<ViewMode>(ViewMode.Month);
+  const [tasks, setTasks] = useState<Task[]>();
+  const columnWidth = 300;
+
   return <Box>hello</Box>;
 };
 
