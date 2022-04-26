787b18a1043ff100c70c870c96325b1f1c36fccc
Ganeshreddy chimmula <ganesh.rc@surya-soft.com>
2022-04-26 14:54:00 +0530

    Move GanntChart ui and logic to seperate page
    -Segregation helps when later including multiple UI components

---
+++ src/pages/GanttChart.tsx
@@ -0,0 +1,24 @@
+import React, { useState, ReactElement } from "react";
+import { Box } from "@mui/material";
+import { Gantt, Task, ViewMode } from "gantt-task-react";
+import { initTasks, getTimeDurationOfProject } from "../services/helperData";
+import "gantt-task-react/dist/index.css";
+
+export const GanttChart = (): ReactElement => {
+  const [view, setView] = useState<ViewMode>(ViewMode.Day);
+  const [tasks, setTasks] = useState<Task[]>(initTasks());
+  const columnWidth = 60;
+  const ganttHeight = 800;
+
+  return (
+    <Box>
+      <Gantt
+        tasks={tasks}
+        viewMode={view}
+        listCellWidth="320px"
+        columnWidth={columnWidth}
+        ganttHeight={ganttHeight}
+      />
+    </Box>
+  );
+};
