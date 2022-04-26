50e6d1b0d6c676adf8953cca72bbeab3ea71ac69
Ganeshreddy chimmula <ganesh.rc@surya-soft.com>
2022-04-26 15:02:27 +0530

    Rename variables and function names
    -Rename identifiers such that comments are not required to understand code

--- src/pages/GanttChart.tsx
+++ src/pages/GanttChart.tsx
@@ -1,12 +1,15 @@
 import React, { useState, ReactElement } from "react";
 import { Box } from "@mui/material";
 import { Gantt, Task, ViewMode } from "gantt-task-react";
-import { initTasks, getTimeDurationOfProject } from "../services/helperData";
+import {
+  getTaskList,
+  getStartAndEndDatesOfProject,
+} from "../services/helperData";
 import "gantt-task-react/dist/index.css";
 
 export const GanttChart = (): ReactElement => {
   const [view, setView] = useState<ViewMode>(ViewMode.Day);
-  const [tasks, setTasks] = useState<Task[]>(initTasks());
+  const [tasks, setTasks] = useState<Task[]>(getTaskList());
   const columnWidth = 60;
   const ganttHeight = 800;
 
